85efc2557d6679df88e1a43acf1697930652bbe0
hu ming <1250446609@qq.com>
2020-02-13 15:45:01 +0800

    安装实例化链码API 以及测试文件调用安装实例化链码代码

--- sdkInit/start.go
+++ sdkInit/start.go
@@ -4,17 +4,20 @@
 package sdkInit
 
 import (
-	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
-	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
 	"fmt"
-	"github.com/hyperledger/fabric-sdk-go/pkg/client/resmgmt"
 	mspclient "github.com/hyperledger/fabric-sdk-go/pkg/client/msp"
-	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/msp"
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/resmgmt"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/errors/retry"
+	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/msp"
+	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
+	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
 
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
+	"github.com/hyperledger/fabric-sdk-go/pkg/fab/ccpackager/gopackager"
+	"github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/common/cauthdsl"
 )
 
-const ChaincodeVersion  = "1.0"
+const ChaincodeVersion = "1.0"
 
 func SetupSDK(ConfigFile string, initialized bool) (*fabsdk.FabricSDK, error) {
 
@@ -57,7 +60,7 @@ func CreateChannel(sdk *fabsdk.FabricSDK, info *InitInfo) error {
 	}
 
 	// SaveChannelRequest holds parameters for save channel request
-	channelReq := resmgmt.SaveChannelRequest{ChannelID:info.ChannelID, ChannelConfigPath:info.ChannelConfig, SigningIdentities:[]msp.SigningIdentity{adminIdentity}}
+	channelReq := resmgmt.SaveChannelRequest{ChannelID: info.ChannelID, ChannelConfigPath: info.ChannelConfig, SigningIdentities: []msp.SigningIdentity{adminIdentity}}
 	// save channel response with transaction ID
 	_, err = resMgmtClient.SaveChannel(channelReq, resmgmt.WithRetry(retry.DefaultResMgmtOpts), resmgmt.WithOrdererEndpoint(info.OrdererOrgName))
 	if err != nil {
@@ -76,4 +79,49 @@ func CreateChannel(sdk *fabsdk.FabricSDK, info *InitInfo) error {
 
 	fmt.Println("peers 已成功加入通道.")
 	return nil
+}
+
+// hanxiaodong
+// QQ群（专业Fabric交流群）：862733552
+func InstallAndInstantiateCC(sdk *fabsdk.FabricSDK, info *InitInfo) (*channel.Client, error) {
+	fmt.Println("开始安装链码......")
+	// creates new go lang chaincode package
+	ccPkg, err := gopackager.NewCCPackage(info.ChaincodePath, info.ChaincodeGoPath)
+	if err != nil {
+		return nil, fmt.Errorf("创建链码包失败: %v", err)
+	}
+
+	// contains install chaincode request parameters
+	installCCReq := resmgmt.InstallCCRequest{Name: info.ChaincodeID, Path: info.ChaincodePath, Version: ChaincodeVersion, Package: ccPkg}
+	// allows administrators to install chaincode onto the filesystem of a peer
+	_, err = info.OrgResMgmt.InstallCC(installCCReq, resmgmt.WithRetry(retry.DefaultResMgmtOpts))
+	if err != nil {
+		return nil, fmt.Errorf("安装链码失败: %v", err)
+	}
+
+	fmt.Println("指定的链码安装成功")
+	fmt.Println("开始实例化链码......")
+
+	//  returns a policy that requires one valid
+	ccPolicy := cauthdsl.SignedByAnyMember([]string{"org1.kevin.kongyixueyuan.com"})
+
+	instantiateCCReq := resmgmt.InstantiateCCRequest{Name: info.ChaincodeID, Path: info.ChaincodePath, Version: ChaincodeVersion, Args: [][]byte{[]byte("init")}, Policy: ccPolicy}
+	// instantiates chaincode with optional custom options (specific peers, filtered peers, timeout). If peer(s) are not specified
+	_, err = info.OrgResMgmt.InstantiateCC(info.ChannelID, instantiateCCReq, resmgmt.WithRetry(retry.DefaultResMgmtOpts))
+	if err != nil {
+		return nil, fmt.Errorf("实例化链码失败: %v", err)
+	}
+
+	fmt.Println("链码实例化成功")
+
+	clientChannelContext := sdk.ChannelContext(info.ChannelID, fabsdk.WithUser(info.UserName), fabsdk.WithOrg(info.OrgName))
+	// returns a Client instance. Channel client can query chaincode, execute chaincode and register/unregister for chaincode events on specific channel.
+	channelClient, err := channel.New(clientChannelContext)
+	if err != nil {
+		return nil, fmt.Errorf("创建应用通道客户端失败: %v", err)
+	}
+
+	fmt.Println("通道客户端创建成功，可以利用此客户端调用链码进行查询或执行事务.")
+
+	return channelClient, nil
 }
